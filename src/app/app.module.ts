89e3a445264d75bff1862fcc05eac6787bac9fd4
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-03-01 00:01:44 +0530

    main menu and top menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
