7111e55c718f45582805fa16fbe31ea9cfd54f28
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-02-29 23:33:17 +0530

    new component - top header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
