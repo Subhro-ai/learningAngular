fd4c35dcf4f9c8f4cb872271129df9ab3e5c3089
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-02-29 23:46:02 +0530

    top header

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 @Component({
-  selector: 'app-top-header',
+  selector: 'top-header',
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.css'
 })
 export class TopHeaderComponent {
-
+  faArrowDown = faArrowDown
 }
