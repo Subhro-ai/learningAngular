8ccc5f0158dce16e1892bfe21279c5b76496343c
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-03-03 04:15:20 +0530

    string interpolation

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-
+  name: string = "iPhone"
+  price: number = 1999
+  colour: string = "Matte Black"
+  discountedPrice: number = this.price - (this.price * 0.2)
 }
