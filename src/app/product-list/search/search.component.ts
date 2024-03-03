1b0ad885334520da675333e709970ed3b4398f2a
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-03-04 02:47:09 +0530

    ngModel

---
+++ src/app/product-list/search/search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  faSearch = faSearch
+  searchText:string = "Mens Wear"
+}
