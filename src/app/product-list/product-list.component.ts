29d241104311f3cae7d50ac986871249712e5bb0
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-03-06 21:49:17 +0530

    change

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,9 +11,10 @@ export class ProductListComponent {
   price : 1999,
   colour: "Matte Black",
   discount : 8.5,
-  inStock : 10,
+  inStock : 50,
   pImage : "assets/71v2jVh6nIL._AC_UF1000,1000_QL80_.jpg"
   }
+  
 
   getDiscount() {
     return this.product.price - this.product.price * this.product.discount / 100
