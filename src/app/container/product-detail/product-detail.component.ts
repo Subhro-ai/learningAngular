1f4061f55023cb7027de5a089dfeeaf755ecb133
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-09-01 02:21:18 +0530

    ngoninit

--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { Products } from '../../Models/Products';
 
 @Component({
   selector: 'product-detail',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
- 
+  @Input() productListComp : ProductListComponent = undefined;
+  product : Products;
+
+  ngOnInit() {
+    this.product = this.productListComp.selectedProduct;
+  }
 }
